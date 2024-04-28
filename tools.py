7a0e319566305a69c62db6a54c494998dde9d115
phi99 <34248917+phi99@users.noreply.github.com>
2024-04-28 19:54:21 +1000

    Delete tools.py

--- tools.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def _hash(passw:str):
-    return (hashlib.sha256(passw.encode())).hexdigest()
-
-def passwcheck(targetpw, hashedpw):
-    targetpw_h=_hash(targetpw)
-    print(targetpw_h)
-    return targetpw_h==hashedpw
